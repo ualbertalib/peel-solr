bca115c4efa73e2c793d61e92a01f54990175d74
T Jenkins <tricia.g.jenkins@gmail.com>
2013-01-17 10:31:26 -0700

    test fails but is run from Ant build.

---
+++ test/ca/ualibraries/dit/martini/solrhome/MartiniConfigTest.java
@@ -0,0 +1,14 @@
+package ca.ualibraries.dit.martini.solrhome;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MartiniConfigTest {
+
+    @Test
+    public void test() {
+	fail("Not yet implemented");
+    }
+
+}
