161dff6ba1f60a2a5d0ed87488283ea51f0d688b
T Jenkins <tricia.g.jenkins@gmail.com>
2013-01-17 13:16:02 -0700

    test configs load and ping status is OK

--- test/ca/ualibraries/dit/martini/solrhome/MartiniConfigTest.java
+++ test/ca/ualibraries/dit/martini/solrhome/MartiniConfigTest.java
@@ -1,14 +1,22 @@
 package ca.ualibraries.dit.martini.solrhome;
 
-import static org.junit.Assert.*;
-
+import org.apache.solr.SolrTestCaseJ4;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class MartiniConfigTest {
+public class MartiniConfigTest extends SolrTestCaseJ4 {
+
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+	initCore("solrconfig.xml", "schema.xml", getFile("solr.home")
+		.getAbsolutePath(), "martini");
+    }
 
     @Test
-    public void test() {
-	fail("Not yet implemented");
+    public void testInitOK() throws Exception {
+	String response = h.query("/admin/ping", req());
+	assertEquals("status should be OK", null,
+		h.validateXPath(response, "//str[@name='status']='OK'"));
     }
 
 }
